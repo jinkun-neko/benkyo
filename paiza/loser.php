8b99e34921b10515fe41bd655acf054ecb0b86dd
jinkun <jin2jin700@gmail.com>
2023-09-17 20:35:43 +0900

    commit

--- paiza/loser.php
+++ paiza/loser.php
@@ -4,9 +4,9 @@
     
     $rank = array();
     for ($i = 0; $i < $N; $i++) {
-        $rank[] = explode(" ", trim(fgets(STDIN)));
-        // 平均の処理
-        $average[] = array_sum($rank[$i]) / count($rank[$i]);
+      $rank[] = explode(" ", trim(fgets(STDIN)));
+      // 平均の処理
+      $average[] = array_sum($rank[$i]) / count($rank[$i]);
     }
     
     // 昇順にアソートして順位順にする
@@ -15,24 +15,23 @@
     // 3位以下の配列だけ残す処理
     $count = 0;
     foreach ($average as $ave_k => $ave_v){
-        unset($average[$ave_k]);
-        // カウントが2になった時点で終了（3位）
-        if($count == 2){
-            break;
-        }
-        $count += 1;
+      unset($average[$ave_k]);
+      // カウントが2になった時点で終了（3位）
+      if($count == 2){
+          break;
+      }
+      $count += 1;
     }
     
     $loser_count = array();
     // foreachして3以上の点数があるバリューに対し
     foreach ($average as $ave_k => $ave_v){
-        foreach ($rank[$ave_k] as $value){
-          if($value <= 3) {
-            $loser_count[] = $ave_k;
-          }  
-        }
+      foreach ($rank[$ave_k] as $value){
+        if($value <= 3) {
+          $loser_count[] = $ave_k;
+        }  
+      }
     }
     
     echo count($loser_count);
-    
 ?>
